bc47d5357ac13108d10d28f5ae73b41539e5bd4d
felix alvarado <felixalvarado653@gmailcom>
2018-05-22 15:19:47 -0700

    finished tictactoe

--- tic_tac_toe/js/main.js
+++ tic_tac_toe/js/main.js
@@ -1,27 +1,27 @@
 const View = require ("./ttt-view.js")
 const Game = require ("../solution/game.js")
-// console.log("I'm here");
+
 $( () => {
 
       const game = new Game();
       const view = new View();
-      console.log(View.prototype);
+
       View.prototype.setupBoard = function() {
       const unorderedList = $("<ul class='list'></ul>");
       const figure = $('figure.ttt');
-      console.log(figure);
-      figure.append(unorderedList);
-      // console.log(game);
 
-      const listItem1 = $("<li class='cell cell1'></li>");
-      const listItem2 = $("<li class='cell cell2'></li>");
-      const listItem3 = $("<li class='cell cell3'></li>");
-      const listItem4 = $("<li class='cell cell4'></li>");
-      const listItem5 = $("<li class='cell cell5'></li>");
-      const listItem6 = $("<li class='cell cell6'></li>");
-      const listItem7 = $("<li class='cell cell7'></li>");
-      const listItem8 = $("<li class='cell cell8'></li>");
-      const listItem9 = $("<li class='cell cell9'></li>");
+      figure.append(unorderedList);
+
+
+      const listItem1 = $("<li class='cell cell1' id='1'></li>");
+      const listItem2 = $("<li class='cell cell2' id='2'></li>");
+      const listItem3 = $("<li class='cell cell3' id='3'></li>");
+      const listItem4 = $("<li class='cell cell4' id='4'></li>");
+      const listItem5 = $("<li class='cell cell5' id='5'></li>");
+      const listItem6 = $("<li class='cell cell6' id='6'></li>");
+      const listItem7 = $("<li class='cell cell7' id='7'></li>");
+      const listItem8 = $("<li class='cell cell8' id='8'></li>");
+      const listItem9 = $("<li class='cell cell9' id='9'></li>");
 
       unorderedList.append(listItem1);
       unorderedList.append(listItem2);
@@ -32,9 +32,50 @@ $( () => {
       unorderedList.append(listItem7);
       unorderedList.append(listItem8);
       unorderedList.append(listItem9);
-      // console.log(unorderedList);
+
       };
+
     view.setupBoard();
 
+    View.prototype.bindEvents = function () {
+    listChild = $('ul li');
+
+    posHash = {
+    1: [0,0],
+    2: [0,1],
+    3: [0,2],
+    4: [1,0],
+    5: [1,1],
+    6: [1,2],
+    7: [2,0],
+    8: [2,1],
+    9: [2,2],
+    }
+
+    listChild.on('click', (e) => {
+    e.preventDefault;
+    targetList = $(e.currentTarget);
+    let id = parseInt(targetList.attr('id'));
+    view.makeMove(id);
+    game.playMove(posHash[id]);
+    if (game.board.isOver()) {
+    let paragraph = $('#over');
+    paragraph.css('font-weight', 300);
+    paragraph.css('font-size', 40);
+    paragraph.css('text-align', 'center');
+    if (view.counter === 9){
+        paragraph.text('Nobody won!! Play again!')
+    }
+      else if (view.counter % 2 === 0){
+      paragraph.text(`Player O is the winner!`);
+      } else {
+        paragraph.text(`Player X is the winner!`);
+      }
+    }
+    });
+
+    }
+
+    view.bindEvents();
 
 });
--- tic_tac_toe/js/ttt-view.js
+++ tic_tac_toe/js/ttt-view.js
@@ -1,9 +1,42 @@
+function markX (square) {
+  const currentSquare = $(`#${square}`);
+  currentSquare.text('X');
+  currentSquare.css('background-color', 'white');
+  currentSquare.css('font-size', '75px');
+  currentSquare.css('font-weight', '500');
+  currentSquare.css('font-family', 'sans-serif');
+  currentSquare.css('text-align', 'center');
+  currentSquare.css('color', 'red');
+}
+
+function markO (square) {
+  const currentSquare = $(`#${square}`);
+  currentSquare.text('O');
+  currentSquare.css('background-color', 'white');
+  currentSquare.css('font-size', '75px');
+  currentSquare.css('font-weight', '500');
+  currentSquare.css('font-family', 'sans-serif');
+  currentSquare.css('text-align', 'center');
+  currentSquare.css('color', 'blue');
+}
+
 class View {
-  constructor(game, $el) {}
+
+  constructor(game, $el) {
+    this.counter = 0;
+  }
 
   bindEvents() {}
 
-  makeMove($square) {}
+  makeMove($square) {
+    if (this.counter % 2 === 0) {
+      markX($square);
+      this.counter += 1;
+    } else {
+      markO($square);
+      this.counter += 1;
+    }
+  }
 
   setupBoard() {}
 }
